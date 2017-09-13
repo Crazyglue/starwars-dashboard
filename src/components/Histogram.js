0f728c4cad104dc258830ebe869a9cbd88a6ac57
Crazyglue <edbz2k2@gmail.com>
2017-09-12 22:29:48 -0500

    Fixup the css one last time

--- src/components/Histogram.js
+++ src/components/Histogram.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { Panel } from 'react-bootstrap'
 import { BarChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar } from 'recharts';
 import _ from 'lodash'
-import './Histogram.css'
 
 class Histogram extends Component {
   render() {
@@ -15,7 +14,7 @@ class Histogram extends Component {
     })
 
     return(
-      <Panel header="Crawl Lengths (by release date)" style={{ height: 370 }}>
+      <Panel className="histogram-panel" header="Crawl Lengths (by release date)" style={{ height: 370 }}>
         <BarChart label={true} data={data} width={220} height={300} margin={{left: 0, bottom: 60}} style={{height: 500}}>
           <XAxis interval={0} tick={{fontSize: 10}} dataKey="title" angle={-45} textAnchor="end" />
           <YAxis height={200} />
